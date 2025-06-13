cb2ed303e3a2904fbae633c522a72c2ad6dfacb7
Synaptek <synapteksoluciones@gmail.com>
2025-06-12 21:40:39 -0500

    ultimo commit

--- synaptekportafolio/src/pages/Contacto.tsx
+++ synaptekportafolio/src/pages/Contacto.tsx
@@ -9,12 +9,18 @@ function Contacto() {
         <p>
           <h2 className='text-amber-200 text-3xl'> .... En Synaptek estamos para ti ......  </h2>
         </p>
-        <p className='text-amber-600 text-2xl'>
+        <p className='text-blue-300 text-2xl'>
+
           ¿Quieres saber más sobre todo lo que podemos hacer por tu empresa?
         </p>
         <p>
           Escríbenos y pronto nos pondremos en contacto.
         </p>
+        
+        <div style={{ flex: 1, minWidth: '300px', padding: '30px' }}>
+         <img src="/contactoi.jpg" alt="" className='rounded-full '/>
+      </div>
+       
       </div>
 
       {/* Contenedor del formulario */}
